bf8c33f71aa683176911317ef747754e28907052
wiper <waipuwiper@gmail.com>
2018-10-19 17:49:55 +0900

    add about

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/foo'
-
-  get 'static_pages/bar'
-
   get 'static_pages/home'
 
   get 'static_pages/help'
+
+  get 'static_pages/about'
 
   root 'application#hello'
 end
